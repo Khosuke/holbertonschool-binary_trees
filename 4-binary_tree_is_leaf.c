49b4e1cfe5a0d39d74544d85f4358ddbc804c0a9
Khosuke <9814@holbertonstudents.com>
2024-12-10 15:25:41 +0100

    changed file format and removed whitespaces

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
  * @node: the node to check
- * 
+ *
  * Return: 1 if node is a leaf, 0 otherwise
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,21 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - 
- * @tree:
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: the tree to mesures
  *
- * Return:
+ * Return: height of the tree or 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
-{	
+{
 	size_t left_height;
 	size_t right_height;
-	
+
 	if (tree == NULL)
 		return (0);
-
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
