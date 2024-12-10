5f2b60c94f143e0a2fcca2dbdf4721e6706c27ca
Yannis <yannismosca@gmail.com>
2024-12-10 15:35:59 +0000

    Push

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,2 +1,48 @@
 #include "binary_trees.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: the tree to mesures
+ *
+ * Return: height of the tree or 0 if tree is NULL
+ */
+int binary_tree_height_(const binary_tree_t *tree)
+{
+	int left_height;
+	int right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->right == NULL || tree->left == NULL)
+		return (1);
+
+	left_height = binary_tree_height_(tree->left);
+
+	right_height = binary_tree_height_(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left;
+
+	int right;
+
+	int nb;
+
+	if (tree == NULL) 
+		return (0);
+
+	left = binary_tree_height_(tree->left);
+
+	right = binary_tree_height_(tree->right);
+
+	nb = left - right;
+
+	return (nb);
+}
 
