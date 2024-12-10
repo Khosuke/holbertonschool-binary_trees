0b0ed5687b4fa684769e27e15b8ead0dd2b7a252
Yannis <yannismosca@gmail.com>
2024-12-10 14:03:37 +0000

    Push

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,2 +1,21 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_leaves - Entry point
+ * @tree: struct
+ * Desc: find leaves
+ * Return: 1 o 2
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->right == NULL && tree->left == NULL)
+	{
+		return (1);
+	}
+
+	return (binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
+}
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,2 +1,19 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - Prints a number
+ * @tree: struct
+ * @func: pointer function
+ * Desc: postorder
+ * Return: Nothing
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return ;
+
+	binary_tree_postorder(tree->left);
+	binary_tree_postorder(tree->right);
+	func(tree->n);
+}
