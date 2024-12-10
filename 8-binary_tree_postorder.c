aecfce6850de6c0c9e642a6727c8659b4c7762df
Khosuke <9814@holbertonstudents.com>
2024-12-10 15:22:09 +0100

    changed CLRF to LF

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,13 +7,12 @@
  * Desc: postorder
  * Return: Nothing
  */
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
-		return ;
+		return;
 
-	binary_tree_postorder(tree->left);
-	binary_tree_postorder(tree->right);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
