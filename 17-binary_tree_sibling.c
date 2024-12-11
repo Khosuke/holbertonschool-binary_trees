3d64decbaa22e526a0e36adbb7be6476dacabb11
Khosuke <9814@holbertonstudents.com>
2024-12-11 12:20:20 +0100

    Added betty documentation comments

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * 
- * 
- * 
- * 
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: node the check
+ *
+ * Return: pointer to the sibling node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
@@ -14,6 +14,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (node->parent->left);
 	if (node->parent->right != node && node->parent->right != NULL)
 		return (node->parent->right);
-	else 
+	else
 		return (NULL);
 }
