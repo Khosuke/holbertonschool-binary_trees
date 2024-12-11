b69ffd647648b95112aeb4f1f7758da695edd263
Yannis <yannismosca@gmail.com>
2024-12-11 11:52:49 +0000

    Push

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,2 +1,51 @@
 #include "binary_trees.h"
 
+/**
+ * height - measures the height of a binary tree
+ * @tree: the tree to mesures
+ * Desc: height
+ * Return: height of the tree or 0 if tree is NULL
+ */
+
+int height(const binary_tree_t *tree)
+{
+	int left_height;
+	int right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	left_height = height(tree->left);
+	right_height = height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
+
+/**
+ * binary_tree_is_perfect - Entry point
+ * @tree: struct
+ * Desc: perfect tree
+ * Return: integer
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left;
+
+	int right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = height(tree->left);
+	right = height(tree->right);
+
+	if (left - right == 0)
+		return (1);
+	else
+		return (0);
+
+}
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,2 +1,39 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_uncle - Entry point
+ * @node: pointer
+ * Desc: find uncle
+ * Return: struct or NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *uncleleft;
+
+	binary_tree_t *uncleright;
+
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent->parent == NULL)
+		return (NULL);
+
+	if (node->parent == NULL)
+		return (NULL);
+
+	if (node->parent == node->parent->parent->right)
+	{
+		uncleleft = node->parent->parent->left;
+		return (uncleleft);
+	}
+
+	if (node->parent == node->parent->parent->left)
+	{
+		uncleright = node->parent->parent->right;
+		return (uncleright);
+	}
+
+	return (NULL);
+}
+
