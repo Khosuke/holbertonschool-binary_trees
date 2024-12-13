7ac1a6f283686aebf6e1cb43ce8386abc3f75e58
Khosuke <9814@holbertonstudents.com>
2024-12-13 23:30:45 +0100

    added recursion to function is perfect to check all subtrees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -42,10 +42,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
 	if (tree->left != NULL && tree->right != NULL)
 	{
 		if (left == right)
-			return (1);
+			if (binary_tree_is_perfect(tree->left) &&
+					binary_tree_is_perfect(tree->right))
+				return (1);
 	}
 	return (0);
 }
